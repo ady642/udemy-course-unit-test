e9ec90efaaa72b91e85c321d8f94f32337f5bbe3
Adrien Haik <ady642@gmail.com>
2023-08-08 13:37:31 +0200

    test: end event as input

--- src/views/About.spec.ts
+++ src/views/About.spec.ts
@@ -2,11 +2,15 @@ import wrapperFactory from "../utils/wrapperFactory";
 import About from "./About.vue";
 import TitleComponent from "../components/TitleComponent.vue";
 import {useAppStore} from "../stores/appStore";
+import {beforeEach} from "vitest";
+
+let wrapper = wrapperFactory(About)
 
 describe('About suite tests', () => {
+    beforeEach(() => {
+        wrapper = wrapperFactory(About)
+    })
     it('should dispatch changeMessage when TitleComponent emits on-mounted', async () => {
-        const wrapper = wrapperFactory(About)
-
         const titleComponentWrapper = wrapper.findComponent(TitleComponent)
 
         const store = useAppStore()
@@ -14,5 +18,24 @@ describe('About suite tests', () => {
         titleComponentWrapper.vm.$emit('on-mounted', 'test')
 
         expect(store.changeMessage).toHaveBeenCalledWith('test')
+    })
+    it('should change the title when the titleComponent emit click event', async () => {
+        const titleComponentWrapper = wrapper.findComponent(TitleComponent)
+
+        await titleComponentWrapper.trigger('click')
+
+        expect(titleComponentWrapper.props('value')).toBe('About !!!')
+    })
+    it('should toggle TitleComponent when the button is clicked', async () => {
+        const button = wrapper.find('button')
+        let titleComponentWrapper = wrapper.findComponent(TitleComponent)
+
+        await button.trigger('click')
+
+        expect(titleComponentWrapper.exists()).toBe(false)
+
+        await button.trigger('click')
+        titleComponentWrapper = wrapper.findComponent(TitleComponent)
+        expect(titleComponentWrapper.exists()).toBe(true)
     })
 })
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,15 +1,24 @@
 <script setup lang="ts">
 import TitleComponent from "../components/TitleComponent.vue";
 import {useAppStore} from "../stores/appStore";
+import {ref} from "vue";
 
 const { changeMessage } = useAppStore()
+
+const title = ref('About')
+const displayTitle = ref(true)
 </script>
 
 <template>
 <TitleComponent
-    value="About"
+    v-if="displayTitle"
+    :value="title"
     @on-mounted="($event: string) => changeMessage($event)"
+    @click="title = 'About !!!'"
 />
+<button
+  @click="displayTitle = !displayTitle"
+>Toggle title !</button>
 </template>
 
 <style scoped>
